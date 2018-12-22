bdf6858e5bd32ecffed125617e529719b1714ad8
alibaba <2322695834@qq.com>
2018-12-22 11:03:00 +0800

    修改为hal库，can.c文件与it.c全部修改

--- CAN—双机通讯/User/can/bsp_can.h
+++ CAN—双机通讯/User/can/bsp_can.h
@@ -1,30 +1,26 @@
 #ifndef __CAN_H
 #define	__CAN_H
 
-#include "stm32f10x.h"
+#include "stm32f1xx.h"
 #include "./usart/bsp_debug_usart.h"
 
 
-#define CANx                       	CAN1
-#define CAN_CLK                    RCC_APB1Periph_CAN1
-#define CAN_RX_IRQ									USB_LP_CAN1_RX0_IRQn
-#define CAN_RX_IRQHandler					USB_LP_CAN1_RX0_IRQHandler
+#define CANx                       CAN1
+#define CAN_CLK_ENABLE()           __HAL_RCC_CAN1_CLK_ENABLE()
+#define CAN_RX_IRQ				         CAN1_RX0_IRQn
+#define CAN_RX_IRQHandler		       CAN1_RX0_IRQHandler
 
-#define CAN_RX_PIN                 GPIO_Pin_8
-#define CAN_TX_PIN                 GPIO_Pin_9
-#define CAN_TX_GPIO_PORT          GPIOB
-#define CAN_RX_GPIO_PORT          GPIOB
-#define CAN_TX_GPIO_CLK           (RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOB)
-#define CAN_RX_GPIO_CLK           RCC_APB2Periph_GPIOB
-
-
-
-
+#define CAN_RX_PIN                 GPIO_PIN_8
+#define CAN_TX_PIN                 GPIO_PIN_9
+#define CAN_TX_GPIO_PORT           GPIOB
+#define CAN_RX_GPIO_PORT           GPIOB
+#define CAN_TX_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOB_CLK_ENABLE()
+#define CAN_RX_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOB_CLK_ENABLE()
 
 
 /*debug*/
 
-#define CAN_DEBUG_ON          1
+#define CAN_DEBUG_ON         1
 #define CAN_DEBUG_ARRAY_ON   1
 #define CAN_DEBUG_FUNC_ON    1
    
@@ -70,9 +66,13 @@ static void CAN_NVIC_Config(void);
 static void CAN_Mode_Config(void);
 static void CAN_Filter_Config(void);
 void CAN_Config(void);
-void CAN_SetMsg(CanTxMsg *TxMessage);
-void Init_RxMes(CanRxMsg *RxMessage);
+void CAN_SetMsg(void);
+void Init_RxMes(void);
 
+extern CAN_HandleTypeDef Can_Handle;
+extern CanTxMsgTypeDef TxMessage;		//发送缓冲区
+extern CanRxMsgTypeDef RxMessage;		//接收缓冲区                                       
+                                                                             
 #endif
 
 
--- CAN—双机通讯/User/main.c
+++ CAN—双机通讯/User/main.c
@@ -20,138 +20,59 @@
 #include "stm32f1xx.h"
 #include "./led/bsp_led.h"
 #include "./usart/bsp_debug_usart.h"
-#include "./key/bsp_exti.h"
-static void SYSCLKConfig_STOP(void);
+#include ".\key\bsp_key.h" 
+#include "./can/bsp_can.h"
+
+__IO uint32_t flag = 0;		 //用于标志是否接收到数据，在中断函数中赋值
 /**
   * @brief  主函数
   * @param  无
   * @retval 无
   */
-int main(void) 
+int main(void)
 {
-	uint32_t SYSCLK_Frequency=0; 
-	uint32_t HCLK_Frequency=0;
-	uint32_t PCLK1_Frequency=0;
-	uint32_t PCLK2_Frequency=0;
-	uint32_t SYSCLK_Source=0;
-	
-	/* 初始化系统时钟为72MHz */
-	SystemClock_Config();
+  /* 配置系统时钟为72 MHz */
+  SystemClock_Config();
 	/* 初始化LED */
 	LED_GPIO_Config();	
 	/* 初始化调试串口，一般为串口1 */
 	DEBUG_USART_Config();	
-	/* 初始化按键为中断模式，按下中断后会进入中断服务函数	*/
-	EXTI_Key_Config(); 
-	
-	printf("\r\n 欢迎使用秉火  STM32 F103 开发板。\r\n");
-    printf("\r\n 秉火F103 停止模式例程\r\n");
-	
-	printf("\r\n 实验说明：\r\n");
+  /*初始化can,在中断接收CAN数据包*/
+  CAN_Config();
 
-	printf("\r\n 1.本程序中，绿灯表示STM32正常运行，红灯表示睡眠状态，蓝灯表示刚从停止状态被唤醒\r\n");
-	printf("\r\n 2.程序运行一段时间后自动进入停止状态，在停止状态下，可使用KEY1或KEY2唤醒\r\n");
-	printf("\r\n 3.本实验执行这样一个循环：\r\n ------》亮绿灯(正常运行)->亮红灯(停止模式)->按KEY1或KEY2唤醒->亮蓝灯(刚被唤醒)-----》\r\n");
-	printf("\r\n 4.在停止状态下，DAP下载器无法给STM32下载程序，\r\n 可按KEY1、KEY2唤醒后下载，\r\n 或按复位键使芯片处于复位状态，然后在电脑上点击下载按钮，再释放复位按键，即可下载\r\n");
+  printf("\r\n 欢迎使用野火  STM32 F103 开发板。\r\n");
+  printf("\r\n 野火F103 CAN通讯实验例程\r\n");
+	
+	printf("\r\n 实验步骤：\r\n");
 
+	printf("\r\n 1.使用导线连接好两个CAN讯设备\r\n");
+	printf("\r\n 2.使用跳线帽连接好:5v --- C/4-5V \r\n");
+	printf("\r\n 3.按下开发板的KEY1键，会使用CAN向外发送0-7的数据包，包的扩展ID为0x1314 \r\n");
+	printf("\r\n 4.若开发板的CAN接收到扩展ID为0x1314的数据包，会把数据以打印到串口。 \r\n");
+	printf("\r\n 5.本例中的can波特率为1MBps，为stm32的can最高速率。 \r\n");
 	while(1)
-	{	
-		/*********执行任务***************************/
-		printf("\r\n STM32正常运行，亮绿灯\r\n");
-
-		LED_GREEN;	
-		HAL_Delay(2000);		
-		/*****任务执行完毕，进入睡眠降低功耗***********/
-
-		printf("\r\n 进入停止模式，亮红灯,按KEY1或KEY2按键可唤醒\r\n");
-
-		//使用红灯指示，进入睡眠状态
-		LED_RED;
-		//暂停滴答时钟，防止通过滴答时钟中断唤醒
-		HAL_SuspendTick();
-		/* 进入停止模式，设置电压调节器为低功耗模式，等待中断唤醒 */
-		HAL_PWR_EnterSTOPMode(PWR_MAINREGULATOR_ON,PWR_STOPENTRY_WFI);
-		//等待中断唤醒  K1或K2按键中断	
-		/***被唤醒，亮蓝灯指示***/
-		LED_BLUE;
-		//根据时钟寄存器的值更新SystemCoreClock变量
-		SystemCoreClockUpdate();
-		//获取唤醒后的时钟状态	
-		SYSCLK_Frequency = HAL_RCC_GetSysClockFreq(); 
-		HCLK_Frequency   = HAL_RCC_GetHCLKFreq();
-		PCLK1_Frequency  = HAL_RCC_GetPCLK1Freq();
-		PCLK2_Frequency  = HAL_RCC_GetPCLK2Freq();
-		SYSCLK_Source    = __HAL_RCC_GET_SYSCLK_SOURCE();
-		//这里由于串口直接使用HSI时钟，不会会影响串口波特率
-		printf("\r\n刚唤醒的时钟状态：\r\n");
-		printf(" SYSCLK频率:%d,\r\n HCLK频率:%d,\r\n PCLK1频率:%d,\r\n PCLK2频率:%d,\r\n 时钟源:%d (0表示HSI，8表示PLLCLK)\n", 
-		SYSCLK_Frequency,HCLK_Frequency,PCLK1_Frequency,PCLK2_Frequency,SYSCLK_Source);		  
-		/* 从停止模式唤醒后配置系统时钟:启用HSE、PLL*/
-		/* 选择PLL作为系统时钟源(HSE和PLL在停止模式下被禁用)*/
-		SYSCLKConfig_STOP();
-		
-		//被唤醒后，恢复滴答时钟			
-		HAL_ResumeTick();
-		//获取重新配置后的时钟状态
-		SYSCLK_Frequency = HAL_RCC_GetSysClockFreq(); 
-		HCLK_Frequency   = HAL_RCC_GetHCLKFreq();
-		PCLK1_Frequency  = HAL_RCC_GetPCLK1Freq();
-		PCLK2_Frequency  = HAL_RCC_GetPCLK2Freq();
-		SYSCLK_Source    = __HAL_RCC_GET_SYSCLK_SOURCE();
-		
-		//重新配置时钟源后始终状态
-		printf("\r\n重新配置后的时钟状态：\r\n");
-		printf(" SYSCLK频率:%d,\r\n HCLK频率:%d,\r\n PCLK1频率:%d,\r\n PCLK2频率:%d,\r\n 时钟源:%d (0表示HSI，8表示PLLCLK)\n", 
-		SYSCLK_Frequency,HCLK_Frequency,PCLK1_Frequency,PCLK2_Frequency,SYSCLK_Source);
-
-		HAL_Delay(2000);	
-			
-		printf("\r\n 已退出停止模式\r\n");
-		//继续执行while循环
+	{
+		/*按一次按键发送一次数据*/
+		if(	Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON)
+		{
+			LED_BLUE;			
+			/* 装载一帧数据 */
+			CAN_SetMsg();
+			/* 开始发送数据 */
+			HAL_CAN_Transmit_IT(&Can_Handle);
+			HAL_Delay(100);
+			LED_RGBOFF;			
+		}
+		if(flag==1)
+		{				
+    	printf("\r\nCAN接收到数据：\r\n");	
+			CAN_DEBUG_ARRAY(Can_Handle.pRxMsg->Data,8); 			
+			flag=0;
+			HAL_Delay(100);
+			LED_GREEN;
+		}	
 	}
 }
-
-/**
-  * @brief  从停止模式唤醒后配置系统时钟:启用HSE、PLL并选择PLL作为系统时钟源。
-
-  * @param  无
-  * @retval 无
-  */
-static void SYSCLKConfig_STOP(void)
-{
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	uint32_t pFLatency = 0;
-
-	/* 启用电源控制时钟 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-
-	/* 根据内部RCC寄存器获取振荡器配置 */
-	HAL_RCC_GetOscConfig(&RCC_OscInitStruct);
-
-	/* 从停止模式唤醒后重新配置系统时钟: 启用HSE和PLL */
-	RCC_OscInitStruct.OscillatorType  = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState        = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState    = RCC_PLL_ON;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-	while(1) { ; }
-	}
-
-	/* 根据内部RCC寄存器获取时钟配置 */
-	HAL_RCC_GetClockConfig(&RCC_ClkInitStruct, &pFLatency);
-
-	/* 选择 PLL 作为系统时钟源, 并配置 HCLK、PCLK1 和 PCLK2时钟分频系数 */
-	RCC_ClkInitStruct.ClockType     = RCC_CLOCKTYPE_SYSCLK;
-	RCC_ClkInitStruct.SYSCLKSource  = RCC_SYSCLKSOURCE_PLLCLK;
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, pFLatency) != HAL_OK)
-	{
-	while(1) { ; }
-	}
-}
-
-
-
 
 /**
   * @brief  System Clock Configuration
