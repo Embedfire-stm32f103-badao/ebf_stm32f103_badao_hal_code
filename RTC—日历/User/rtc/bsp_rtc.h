7b5e26afa1eb0c55c087ef4312e92a1a9b701e87
alibaba <2322695834@qq.com>
2018-12-24 11:22:42 +0800

    HAL库很强大，关于时间计算函数都封装很完善

--- RTC—日历/User/rtc/bsp_rtc.h
+++ RTC—日历/User/rtc/bsp_rtc.h
@@ -1,41 +1,35 @@
 
-
 #ifndef __RTC_H
 #define	__RTC_H
 
+#include "stm32f1xx.h"
 
-#include "stm32f10x.h"
-#include "./rtc/bsp_calendar.h"
-#include "./rtc/bsp_date.h"
-
-//是否使用LCD显示日期
+//是否使用LCD显示
 #define USE_LCD_DISPLAY
 
+extern RTC_HandleTypeDef Rtc_Handle;
+// 时钟源宏定义
+#define RTC_CLOCK_SOURCE_LSE      
+//#define RCC_OSCILLATORTYPE_LSI  
+ 
+#define HOURS                     0x12          // 0~23
+#define MINUTES                   0x12          // 0~59
+#define SECONDS                   0x12          // 0~59
 
-//使用LSE外部时钟 或 LSI内部时钟
-//#define RTC_CLOCK_SOURCE_LSE      
-#define RTC_CLOCK_SOURCE_LSI
 
+// 日期宏定义  其中WEEKDAY为无效宏定义，只要给出年月日，HAL库会自动计算出 WEEKDAY
+#define WEEKDAY                   6           // 1~7
+#define DATE                      0x24        // 1~31
+#define MONTH                     0x12        // 1~12
+#define YEAR                      0x18        // 0~99
 
-
-#define RTC_BKP_DRX          BKP_DR1
+// 备份域寄存器宏定义
+#define RTC_BKP_DRX          RTC_BKP_DR1
 // 写入到备份寄存器的数据宏定义
-#define RTC_BKP_DATA         0xA5A5
-
-//北京时间的时区秒数差
-#define TIME_ZOOM						(8*60*60)
-
-
-/* 如果定义了下面这个宏的话,PC13就会输出频率为RTC Clock/64的时钟 */   
-//#define RTCClockOutput_Enable  /* RTC Clock/64 is output on tamper pin(PC.13) */
-
-void RTC_NVIC_Config(void);
-void RTC_Configuration(void);
-void Time_Regulate_Get(struct rtc_time *tm);
-void Time_Adjust(struct rtc_time *tm);
-void Time_Display(uint32_t TimeVar,struct rtc_time *tm);
-void Time_Show(struct rtc_time *tm);
-static uint8_t USART_Scanf(uint32_t value);
-void RTC_CheckAndConfig(struct rtc_time *tm);
+#define RTC_BKP_DATA         0X32F2 
+                                  
+void RTC_CalendarConfig(void);
+void RTC_TimeAndDate_Show(void);
+void HAL_RTC_MspInit(RTC_HandleTypeDef *hrtc);
 
 #endif /* __XXX_H */
