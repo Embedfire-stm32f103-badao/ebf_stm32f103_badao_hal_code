c418f37192d0eff1f7aab472a8b8532158d41c2f
hejiawen <hjw0415@utlook.com>
2019-10-23 17:08:41 +0800

    升级hal库，测试正常

--- TIM—通用定时器-输入捕获/Libraries/STM32F1xx_HAL_Driver/Inc/Legacy/stm32f1xx_hal_can_ex_legacy.h
+++ TIM—通用定时器-输入捕获/Libraries/STM32F1xx_HAL_Driver/Inc/Legacy/stm32f1xx_hal_can_ex_legacy.h
@@ -1,9 +1,7 @@
 /**
   ******************************************************************************
-  * @file    stm32f1xx_hal_can_ex.h
+  * @file    stm32f1xx_hal_can_ex_legacy.h
   * @author  MCD Application Team
-  * @version V1.1.1
-  * @date    12-May-2017
   * @brief   Header file of CAN HAL Extension module.
   ******************************************************************************
   * @attention
@@ -36,8 +34,8 @@
   */ 
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F1xx_HAL_CAN_EX_H
-#define __STM32F1xx_HAL_CAN_EX_H
+#ifndef __STM32F1xx_HAL_CAN_EX_LEGACY_H
+#define __STM32F1xx_HAL_CAN_EX_LEGACY_H
 
 #ifdef __cplusplus
  extern "C" {
@@ -141,6 +139,6 @@ typedef struct
 }
 #endif
 
-#endif /* __STM32F1xx_HAL_CAN_EX_H */
+#endif /* __STM32F1xx_HAL_CAN_EX_LEGACY_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
